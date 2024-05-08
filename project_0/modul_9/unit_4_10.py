28fda09aefa5502365ee9e67959684cea7789551
almazrl80 <almazrl80@gmail.com>
2024-05-08 20:55:07 +0300

    solve module 9

---
+++ project_0/modul_9/unit_4_10.py
@@ -0,0 +1,15 @@
+tasks = [(36871, 'office', False),
+(40690, 'office', False),
+(35364, 'voltage', False),
+(41667, 'voltage', True),
+(33850, 'office', False)]
+from collections import deque, defaultdict
+def task_manager(tasks):
+    servers = defaultdict(deque)
+    for task in tasks:
+        if task[-1]:
+            servers[task[1]].appendleft(task[0])
+        else:
+            servers[task[1]].append(task[0])
+    return servers
+print(task_manager(tasks))
---
+++ project_0/modul_9/unit_4_8.py
@@ -0,0 +1,18 @@
+# from hidden import center,south,north
+# def reveal(shop):
+#     lst=[]
+#     for i in shop:
+#         for j in i:
+#             lst.append(j)
+#     return lst
+# lst_c=reveal(center)
+# lst_s=reveal(south)
+# lst_n=reveal(north)
+# from collections import Counter 
+# c=Counter(lst_c)
+# s=Counter(lst_s)
+# n=Counter(lst_n)
+# #print(n.most_common()[-1])
+# # print('north',n.most_common(3))
+# x=c+n+s
+# print(x.most_common(2))
